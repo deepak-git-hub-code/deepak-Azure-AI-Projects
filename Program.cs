7451a81c08deb0c121beee0c62e79fdcb4843893
deepak-git-hub-code <deepaksinghcs29@outlook.com>
2026-02-07 16:42:28 +0530

    Update Program.cs file with new content

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Threading.Tasks;
 using Azure.AI.Projects;
 using Azure.Identity;
@@ -7,29 +7,26 @@ using System.ClientModel.Primitives;
 
 internal class Program
 {
-     static async Task  Main(string[] args)
+    static async Task Main(string[] args)
     {
         Console.WriteLine("Hello, World!");
 
-        //1. Azure AI endpoint
+        // 1. Azure AI endpoint
         var endpoint = new Uri(
             "https://resource.services.ai.azure.com/api/projects/yourproject"
-            );
-        
+        );
 
-        //2. . Use DefaultAzureCredential (picks up your MFA CLI login automatically)
-
+        // 2. Use DefaultAzureCredential (picks up your MFA CLI login automatically)
         var cred = new DefaultAzureCredential();
 
         // 3. Create Foundry client
         var client = new AIProjectClient(endpoint, cred);
-        Console.WriteLine(" client created");
+        Console.WriteLine("Client created");
 
-
+        // 4. List deployments
         await foreach (var deployment in client.Deployments.GetDeploymentsAsync())
         {
             Console.WriteLine($"Deployment: {deployment.Name}");
         }
-
     }
 }
